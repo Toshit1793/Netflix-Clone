969a15cb3174a772347427e1d30ec57958b65975
Toshit1793 <toshitprasad3@gmail.com>
2025-10-01 22:48:16 +0530

    Fix TitleCards component and improve event handling

--- src/components/TitileCards/TitleCards.jsx
+++ src/components/TitileCards/TitleCards.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import './TitleCards.scss'
+import React, { useRef, useEffect } from 'react'
+import './TitleCards.css'
+import card_data from '../../assets/cards/Cards_data.js'
 
 const TitleCards = () => {
+  const cardsRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    const currentRef = cardsRef.current;
+    currentRef.addEventListener('wheel', handleWheel);
+
+    return () => {
+      currentRef.removeEventListener('wheel', handleWheel);
+    };
+  }, []);
+
   return (
     <div className='titleCards'>
-      
+      <h2>Popular on Netflix</h2>
+      <div className='card-list' ref={cardsRef}>
+        {card_data.map((cards, index) => (
+          <div className='card' key={index}>
+            <img src={cards.image} alt={cards.title} />
+            <p>{cards.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
-export default TitileCards
+export default TitleCards
